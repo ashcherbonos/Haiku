4fddb7f606b582451f1b98d82c32810bb1d96358
ashcherbonos <30783567+ashcherbonos@users.noreply.github.com>
2026-04-07 21:13:23 +0200

    refactor brackets

--- Brackets/solution.js
+++ Brackets/solution.js
@@ -1,7 +1,8 @@
 const
-open = s => ['{','[','('].indexOf(s) + 1, 
-close = s => ['}',']',')'].indexOf(s) + 1,
-solution = (S) => (
+indx = s => c => s.split('').indexOf(c) + 1,
+open = indx('{[('), 
+close = indx('}])'),
+solution = S => (
     stack => (
         S.split('').forEach(s => open(s) && stack.push(open(s)) || close(s) && stack.pop() !== close(s) && stack.push(-1)),
         1 * (stack.length === 0)
