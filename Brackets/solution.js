4da8df5b5d0a2e07e3924271436bfecb0d415942
ashcherbonos <30783567+ashcherbonos@users.noreply.github.com>
2026-04-07 21:15:30 +0200

    Update solution.js

--- Brackets/solution.js
+++ Brackets/solution.js
@@ -1,9 +1,7 @@
 const
 indx = s => c => s.split('').indexOf(c) + 1,
-open = indx('{[('), 
-close = indx('}])'),
-solution = S => (
-    stack => (
+open = indx('{[('), close = indx('}])'),
+solution = S => ( stack => (
         S.split('').forEach(s => open(s) && stack.push(open(s)) || close(s) && stack.pop() !== close(s) && stack.push(-1)),
         1 * (stack.length === 0)
     ))([]);
